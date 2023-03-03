9514e57c89423eb2ce5aea1183306fd5bb698c81
felipemalli <felipevahia@gmail.com>
2023-03-03 00:59:39 -0300

    refactor: remove unnecessary files

--- src/routes/blogPostRouter.js
+++ src/routes/blogPostRouter.js
@@ -4,9 +4,9 @@ const authMiddleware = require('../middlewares/authMiddleware');
 const { createPostValidation, updatePostValidation } = require('../middlewares/postMiddleware');
 
 router.post('/', authMiddleware, createPostValidation, blogPostController.create);
-router.get('/', authMiddleware, blogPostController.getAll);
 router.get('/search', authMiddleware, blogPostController.getBySearch);
 router.get('/:id', authMiddleware, blogPostController.getById);
+router.get('/', authMiddleware, blogPostController.getAll);
 router.put('/:id', authMiddleware, updatePostValidation, blogPostController.update);
 router.delete('/:id', authMiddleware, blogPostController.remove);
 
